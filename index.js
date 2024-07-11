99110b1ab924e61bbe87ac071d4eaea2e0996ce1
afreenx <a3pmfatima@gmail.com>
2024-07-11 12:21:11 -0500

    challenge 7 top 2 game display

--- index.js
+++ index.js
@@ -182,7 +182,15 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [mostPledged, secondMostPledged, ...topGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const mostPledgedE = document.createElement('p');
+mostPledgedE.innerHTML = mostPledged.name;
+firstGameContainer.appendChild(mostPledgedE);
 
 // do the same for the runner up item
+const secondMostPledgedE = document.createElement('p');
+secondMostPledgedE.innerHTML = secondMostPledged.name;
+secondGameContainer.appendChild(secondMostPledgedE);
+
